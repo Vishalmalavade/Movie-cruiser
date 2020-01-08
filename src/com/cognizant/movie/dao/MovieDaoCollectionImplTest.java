6005e47665840c188952785c944f54ce34ba406d
Malavade <835513@cognizant.com>
2020-01-08 16:28:53 +0530

    java

--- src/com/cognizant/movie/dao/MovieDaoCollectionImplTest.java
+++ src/com/cognizant/movie/dao/MovieDaoCollectionImplTest.java
@@ -19,8 +19,8 @@ public class MovieDaoCollectionImplTest {
 
     public static void testEditMovie() {
         System.out.println("Edit Movie");
-        Movie movielist = new Movie(1, "Watch and Enjoy", 123450L, true,
-                DateUtil.convertToDate("03/01/2019"), "Action", false);
+        Movie movielist = new Movie(1, "Spider Man", 1234507897L, true,
+                DateUtil.convertToDate("03/01/2019"), "Comedy", false);
         moviedao.modifyMovieList(movielist);
 
     }
@@ -29,7 +29,7 @@ public class MovieDaoCollectionImplTest {
         MovieDao moviedao = new MovieDaoCollectionImpl();
         List<Movie> movie = moviedao.getMovieListAdmin();
         System.out.println("Admin");
-        System.out.printf("%-10s%-20s%-22s%-15s%-18s%-23s%-20s\n", "Id", "Title", "Box Office",
+        System.out.printf("%-10s%-24s%-17s%-14s%-22s%-20s%-20s\n", "Id", "Title", "Box Office",
                 "Active", "Date Of Launch", "Genre", "Has Teaser");
         for (Movie movies : movie) {
             System.out.println(movies);// invokes toString();
@@ -41,8 +41,8 @@ public class MovieDaoCollectionImplTest {
     public static void testGetMovieListCustomer() {
         List<Movie> movie = moviedao.getMovieListCustomer();
         System.out.println("Customer");
-        System.out.printf("%-10s%-20s%-22s%-15s%-18s%-23s%-20s\n", "Id", "Title", "Box Office",
-                "Active", "Date Of Launch", "Genre", "HasTeaser");
+        System.out.printf("%-10s%-24s%-17s%-14s%-22s%-20s%-20s\n", "Id", "Title", "Box Office",
+                "Active", "Date Of Launch", "Genre", "Has Teaser");
         for (Movie movies2 : movie) {
             System.out.println(movies2);
 
@@ -51,7 +51,7 @@ public class MovieDaoCollectionImplTest {
 
     public static void testGetMovie() {
         System.out.println("Get Movies");
-        System.out.printf("%-10s%-20s%-22s%-15s%-18s%-23s%-20s\n", "Id", "Title", "Box Office",
+        System.out.printf("%-10s%-24s%-17s%-14s%-22s%-20s%-20s\n", "Id", "Title", "Box Office",
                 "Active", "Date of Launch", "Genre", "Has Teaser");
         Movie movie = moviedao.getMovieById(3);
         System.out.println(movie);// invokes toString()
